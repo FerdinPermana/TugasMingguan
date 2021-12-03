a0d0c6dbe0433dcbce48753e9e720128d525b558
FerdinPermana <94943661+FerdinPermana@users.noreply.github.com>
2021-12-03 22:24:43 +0700

    Add files via upload

---
+++ src/com/sqa/No1.java
@@ -0,0 +1,28 @@
+package com.sqa.no1;
+
+import java.util.Scanner;
+
+public class No1 {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Masukan nilai maksimal deret = ");
+		int batas = in.nextInt();
+		
+		cetakFibonacci(batas);
+
+	}
+	
+	static void cetakFibonacci(int batas) {
+		int fib[] = new int[batas];
+		fib[0]=0;
+		fib[1]=1;
+		for(int i=2; i<batas; i++) {
+			fib[i] = fib[i-1] + fib[i-2];
+		}
+		for (int i=0; i<=batas; i++) {
+			System.out.print(fib[i]+  " ");
+		}
+	}
+
+}
